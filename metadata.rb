766f80f5bdc16ffb534aaba0b70316de491f95e5
Thomas Boerger <tboerger@tbpro.de>
2013-10-17 17:27:33 +0200

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name "sqlite"
+maintainer "Thomas Boerger"
+maintainer_email "tboerger@tbpro.de"
+license "Apache 2.0"
+description "Installs/Configures sqlite"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version "0.0.1"
+recipe "sqlite", "Installs/Configures sqlite"
+
+supports "suse"
